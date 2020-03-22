bf4a18a8604ead8619ef19aee3d410961e1f51eb
misachi <misachibrian@gmail.com>
2020-03-22 16:30:28 +0300

    Allow search and delete items for linked list

--- linked_list.c
+++ linked_list.c
@@ -27,6 +27,36 @@ void insert(struct list *list, int num) {
     list->root = node;
 }
 
+struct node *search(struct list *list, int num) {
+    struct node *node = list->root;
+
+    while (node)
+    {
+        if (num == node->data) {
+            return node;
+        }
+        node = node->next;
+    }
+    return node;
+}
+
+void delete_item(struct list *list, int num) {
+    struct node *node = search(list, num);
+    if (!node)
+        return; // Item does not exist
+
+    if (node->prev)
+        node->prev->next = node->next;
+    else
+        list->root = node->next;
+
+    if (node->next)
+        node->next->prev = node->prev;
+
+    free(node);
+    list->size--; // Decrease count by 1
+}
+
 void clean_up(struct node *node) {
     while (node != NULL)
     {
@@ -52,6 +82,10 @@ int main(void) {
     insert(list, 3);
     insert(list, 2);
 
+    delete_item(list, 2);
+    struct node* node = search(list, 2);
+    printf("Found: %d\n", (node) ? node->data : -1);
+    printf("Traversing...\n");
     list_traverse(list->root);
     printf("Size: %d\n", list->size);
     clean_up(list->root);
